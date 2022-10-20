f8337aacd023712c7d155a679cbee0176df3b3f6
jj_ranalli <jacopo.ranalli@mail.polimi.it>
2022-10-21 01:24:45 +0200

    fixed lib/prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client"
 
-// // add prisma to the NodeJS global type
-// interface CustomNodeJsGlobal extends NodeJS.Global {
-//   prisma: PrismaClient
-// }
+// add prisma to the NodeJS global type
+interface CustomNodeJsGlobal extends NodeJS.Global {
+  prisma: PrismaClient
+}
 
-// // Prevent multiple instances of Prisma Client in development
-// declare const global: CustomNodeJsGlobal
+// Prevent multiple instances of Prisma Client in development
+declare const global: CustomNodeJsGlobal
 
 const prisma = new PrismaClient()
 
-// if (process.env.NODE_ENV === "development") global.prisma = prisma
+if (process.env.NODE_ENV === "development") global.prisma = prisma
 
 export default prisma
